56788cffee3e75351af38983f79634adb820c259
Rob Daniels <rob@danielscorporation.com>
2014-06-12 06:50:41 -0400

    starting iterative solution

--- tower_of_hanoi/lib/tower_of_hanoi.rb
+++ tower_of_hanoi/lib/tower_of_hanoi.rb
@@ -1,18 +1,51 @@
 module TowerOfHanoi
   class Tower
 
-
+		# iterative solution
 		def init(number_of_pieces)
 			rod0 = []
 			rod1 = []
 			rod2 = []
+			last_move = ""
+			move_1_2 = false
+			move_1_3 = false
+			move_2_1 = false
+			move_2_3 = false
+			move_3_1 = false
+			move_3_2 = false
 
-			rod0 = build_first_tower(number_of_pieces)
+			solution_array = order_pieces(number_of_pieces)
+			rod0 = solution_array
+
+			# first move
+			(number_of_pieces.odd?) ? rod2 << rod0.last : rod1 << rod0.last
+ 			rod0 = rod0[0..-2]
+
+ 			# loop through all moves after first
+ 			begin
+ 				rod2 = solution_array
+ 			end while rod2 != solution_array
+
+ 			# results
+ 			puts "rod0 = " + rod0.to_s
+ 			puts "rod1 = " + rod1.to_s
+ 			puts "rod2 = " + rod2.to_s
 		end
 
-		def build_first_tower(number_of_pieces)
+		def order_pieces(number_of_pieces)
 			(1..number_of_pieces).to_a.reverse 
 		end
+
+
+
+		def valid_even_odd(a,b)
+			return false if a.even? && b.even?
+			return false if a.odd? && b.odd?
+			true
+		end
+
+
+
 
 		def smallest_piece(p0, p1, p2)
 			p0 = 99999 if p0 == nil
@@ -20,6 +53,16 @@ module TowerOfHanoi
 			p2 = 99999 if p2 == nil
 			a = [p0, p1, p2]
 			a.index(a.min)
+		end
+
+		def next_smallest_piece(p0, p1, p2)
+			p0 = 99999 if p0 == nil
+			p1 = 99999 if p1 == nil
+			p2 = 99999 if p2 == nil
+			a = [p0, p1, p2].sort
+			return 0 if a[1] == p0
+			return 1 if a[1] == p1
+			return 2 if a[1] == p2
 		end
   end
 end
--- tower_of_hanoi/spec/tower-spec.rb
+++ tower_of_hanoi/spec/tower-spec.rb
@@ -5,7 +5,7 @@ module TowerOfHanoi
 
   	it 'builds the first tower' do
   		my_tower = Tower.new
-  		rod1 = my_tower.build_first_tower(5)
+  		rod1 = my_tower.order_pieces(5)
   		expect(rod1).to eq([5,4,3,2,1])
   	end
 
@@ -18,5 +18,18 @@ module TowerOfHanoi
 			expect(rod).to eq(1)
   	end
 
+    it 'selects mid piece' do
+      my_tower = Tower.new
+      rod0 = [4,2,3]
+      rod1 = [5,6,2]
+      rod2 = []
+      rod = my_tower.next_smallest_piece(rod0.last, rod1.last, rod2.last)
+      expect(rod).to eq(0)
+    end
+
+    it 'shows result' do
+      my_tower = Tower.new
+      my_tower.init(5)
+    end
   end
 end
